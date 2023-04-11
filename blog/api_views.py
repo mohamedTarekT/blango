18f55d1c3602a5ddc6fc85430ad641fa2159f803
tarek <mohamed.tarek@treyd.io>
2023-04-11 23:52:32 +0200

    add generics

--- blog/api_views.py
+++ blog/api_views.py
@@ -5,9 +5,14 @@ from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from blog.api.serializers import PostSerializer
+from rest_framework.views import APIView
 
+from blog.api.serializers import PostSerializer
+from rest_framework.response import Response
 from blog.models import Post
+from rest_framework import generics
+
+from rest_framework.decorators import api_view
 
 
 # def post_to_dict(post):
@@ -24,38 +29,78 @@ from blog.models import Post
 #     }
 
 
-@csrf_exempt
-def post_list(request):
-    if request.method == "GET":
-        posts = Post.objects.all()
-        return JsonResponse({"data": PostSerializer(posts, many=True).data})
-    elif request.method == "POST":
-        post_data = json.loads(request.body)
-        serializer = PostSerializer(data=post_data)
-        serializer.is_valid(raise_exception=True)
-        post = serializer.save()
-        return HttpResponse(
-            status=HTTPStatus.CREATED,
-            headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-        )
+# @csrf_exempt
+# @api_view(["GET", "POST"])
+# def post_list(request, format = None):
+#     if request.method == "GET":
+#         posts = Post.objects.all()
+#         return JsonResponse({"data": PostSerializer(posts, many=True).data})
+#     elif request.method == "POST":
+#         post_data = json.loads(request.body)
+#         serializer = PostSerializer(data=post_data)
+#         serializer.is_valid(raise_exception=True)
+#         post = serializer.save()
+#         return HttpResponse(
+#             status=HTTPStatus.CREATED,
+#             headers={"Location": reverse("api_post_detail", args=(post.pk,))},
+#         )
+#
+#     return HttpResponseNotAllowed(["GET", "POST"])
 
-    return HttpResponseNotAllowed(["GET", "POST"])
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
-@csrf_exempt
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
 
-    if request.method == "GET":
-        return JsonResponse(PostSerializer(post).data)
-    elif request.method == "PUT":
-        post_data = json.loads(request.body)
-        serializer = PostSerializer(post, data=post_data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
-    elif request.method == "DELETE":
-        post.delete()
-        return HttpResponse(status=HTTPStatus.NO_CONTENT)
 
-    return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
+
+# @csrf_exempt
+# @api_view(["GET", "PUT", "DELETE"])
+# def post_detail(request, pk, format =None):
+#     # post = get_object_or_404(Post, pk=pk)
+#     try:
+#       post = Post.objects.get(pk=pk)
+#     except Post.DoesNotExist:
+#       return Response(status=HTTPStatus.NOT_FOUND)
+#     if request.method == "GET":
+#         return Response(PostSerializer(post).data)
+#     elif request.method == "PUT":
+#         # post_data = json.loads(request.body)
+#         # serializer = PostSerializer(post, data=post_data)
+#         serializer = PostSerializer(post, data=request.data)
+#
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(status=HTTPStatus.NO_CONTENT)
+#     elif request.method == "DELETE":
+#         post.delete()
+#         return Response(status=HTTPStatus.NO_CONTENT)
+#
+#     return HttpResponseNotAllowed(["GET", "PUT", "DELETE"])
+
+# class PostDetail(APIView):
+#     @staticmethod
+#     def get_post(pk):
+#         return get_object_or_404(Post, pk=pk)
+#
+#     def get(self, request, pk, format=None):
+#         post = self.get_post(pk)
+#         return Response(PostSerializer(post).data)
+#
+#     def put(self, request, pk, format=None):
+#         post = self.get_post(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(status=HTTPStatus.NO_CONTENT)
+#         return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         post = self.get_post(pk)
+#         post.delete()
+#         return Response(status=HTTPStatus.NO_CONTENT)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
